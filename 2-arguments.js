6990ab047aa8dc961e2e77fb5df573b6d2c5db72
Kiptum <duncankipkoech12@gmail.com>
2025-07-12 20:34:02 +0300

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,8 @@
 const script = process.script.length;
 
-if (scriptLength === 2) {
+if (scriptLength === 0) {
     console.log("No argument");
-} else if (scriptLength === 3) {
+} else if (scriptLength === 1) {
     console.log("Argument found");
 } else {
     console.log("Arguments found");
